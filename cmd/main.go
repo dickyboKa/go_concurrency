b19847440f3f13232a92ef212cd27c8484bde7c6
dickyboKa <dicky.kamar14@gmail.com>
2020-05-17 12:57:09 +0700

    closed channel, make new channel and range over value

--- cmd/main.go
+++ cmd/main.go
@@ -11,4 +11,24 @@ func main() {
 	}()
 	salutation, ok := <-stringStream
 	fmt.Printf("(%v): %v\n", ok, salutation)
+
+	// try to read from closed channel
+	intStream := make(chan int)
+	close(intStream)
+	i, ok := <- intStream
+	fmt.Printf("(%v): %v\n", ok, i)
+
+	// range/loop over channel
+	intStream = make(chan int)
+	go func() {
+		defer close(intStream)
+		for i := 1; i <= 5; i++ {
+			intStream <- i
+		}
+	}()
+
+	for i := range intStream {
+		fmt.Printf("%v ", i)
+	}
+	fmt.Println()
 }
